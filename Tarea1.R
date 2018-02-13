0c48cb1c4a5389905a775e8adb5803d356d06d77
KevinChica1128 <kevin.chica@correounivalle.edu.co>
2018-02-13 18:20:27 -0500

    actualización correlación covariabless

--- Tarea1.R
+++ Tarea1.R
@@ -74,6 +74,19 @@ hist(cadata$Longitud, freq = F,col="gray52",main="Histograma de la longitud",yla
 curve(dnorm(x, mean(cadata$Longitud), sd(cadata$Longitud)), col = 2, lty = 2, lwd = 2, add=T)
 plot(density(cadata$Longitud),main="Densidad de la longitud",col="darkblue",ylab = "densidades",xlab = "longitud")
 
+#Relaciones entre variables explicativas:
+#Población y hogares:
+x11()
+plot(cadata$Poblacion,cadata$Hogares,xlab = 'Población',ylab = 'Hogares',main='Gráfico de puntos entre la variable "Población" y la variable "Hogares" ')
+cor(cadata$Poblacion,cadata$Hogares) #correlación de Pearson
+cor(cadata$Poblacion,cadata$Hogares,method='spearman') #Correlación de Spearman
+#Total de habitaciones y Total de dormitorios:
+x11()
+plot(cadata$Total_de_habitaciones,cadata$Total_de_dormitorios,xlab = 'Total de habitaciones',ylab = 'Total de dormitorios',main='Gráfico de puntos entre la variable "total de habitaciones" y la variable "total de dormitorios" ')
+cor(cadata$Total_de_habitaciones,cadata$Total_de_dormitorios) #correlación de Pearson
+cor(cadata$Total_de_habitaciones,cadata$Total_de_dormitorios,method='spearman') #Correlación de Spearman
+
+
 #Estimación y ajuste del modelo:
 Regresion<- lm(cadata$Valor_medio_de_la_casa ~ cadata$Ingreso_medio+cadata$Edad_media_de_la_vivienda+
                cadata$Total_de_habitaciones+cadata$Total_de_dormitorios+cadata$Poblacion+cadata$Hogares+
